f1c9cd64c4066d881807f4821790a4b7ee832d49
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 17:46:33 +0000

    Deploy PilotPaul/blog.github.io to github.com/PilotPaul/blog.github.io.git:gh-pages

--- dist/music.js
+++ dist/music.js
@@ -6,38 +6,38 @@ const ap = new APlayer({
       {
         name: "恋爱循环",
         artist: '花泽香菜',
-        url: '../music/mp3/LoveScenario.mp3',
-        cover: '../music/pic/LoveScenario.jpg',
+        url: '/music/mp3/LoveScenario.mp3',
+        cover: '/music/pic/LoveScenario.jpg',
       },
       {
         name: 'Zeze',
         artist: 'IU',
-        url: '../music/mp3/IU-Zeze.mp3',
-        cover: '../music/pic/zeze-iu.jpg',
+        url: '/music/mp3/IU-Zeze.mp3',
+        cover: '/music/pic/zeze-iu.jpg',
       },
       {
         name: 'BIBI',
         artist: 'IU',
-        url: '../music/mp3/李知恩_IU_-삐삐（BBIBBI）.mp3',
-        cover: '../music/pic/BIBI-iu.jpg',
+        url: '/music/mp3/李知恩_IU_-삐삐（BBIBBI）.mp3',
+        cover: '/music/pic/BIBI-iu.jpg',
       },
       {
         name: 'unknown',
         artist: 'IU',
-        url: '../music/mp3/IU-잼잼.mp3',
-        cover: '../music/pic/Friday-iu.jpg',
+        url: '/music/mp3/IU-잼잼.mp3',
+        cover: '/music/pic/Friday-iu.jpg',
       },
       {
         name: 'unknown',
         artist: 'IU',
-        url: '../music/mp3/IU-팔레트-_Feat.-G-DRAGON_.mp3',
-        cover: '../music/pic/iu.jpg',
+        url: '/music/mp3/IU-팔레트-_Feat.-G-DRAGON_.mp3',
+        cover: '/music/pic/iu.jpg',
       },
 	  {
         name: 'unknown',
         artist: 'IU',
-        url: '../music/mp3/IU-마쉬멜로우.mp3',
-        cover: '../music/pic/iu.jpg',
+        url: '/music/mp3/IU-마쉬멜로우.mp3',
+        cover: '/music/pic/iu.jpg',
       }
     ]
 });
